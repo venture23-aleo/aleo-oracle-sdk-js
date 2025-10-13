798258110896f8cbee15f5f61a6f751e4c6c88c5
Debendra Oli <debendraoli@pm.me>
2025-10-13 11:09:29 +0545

    add: support for self-signed(mTLS) certificate

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,48 @@
 import OracleClient from './client';
 
 import {
-  DEFAULT_TIMEOUT_MS,
-  DEFAULT_NOTARIZATION_HEADERS,
   DEFAULT_FETCH_OPTIONS,
-  DEFAULT_NOTARIZATION_OPTIONS,
   DEFAULT_NOTARIZATION_BACKENDS,
+  DEFAULT_NOTARIZATION_HEADERS,
+  DEFAULT_NOTARIZATION_OPTIONS,
+  DEFAULT_TIMEOUT_MS,
   DEFAULT_VERIFICATION_BACKEND,
 } from './defaults';
 
 import {
   AttestationError,
-  DebugAttestationError,
   AttestationIntegrityError,
+  DebugAttestationError,
 } from './errors';
 
 import {
-  ClientConfig,
-  CustomBackendConfig,
-  CustomBackendAllowedFetchOptions,
-
   AttestationRequest,
   AttestationResponse,
+  ClientConfig,
+  CustomBackendAllowedFetchOptions,
+  CustomBackendConfig,
   DebugRequestResponse,
   EnclaveInfo,
   HeaderDict,
   InfoOptions,
+  NitroDocument,
+  NitroInfo,
+  NitroReportExtras,
   NotarizationOptions,
   OracleData,
   PositionInfo,
   ProofPositionalInfo,
   SgxInfo,
-  NitroInfo,
-  NitroDocument,
-  NitroReportExtras,
+  TlsConfig,
 } from './types';
 
 export {
-  OracleClient,
 
-  DEFAULT_TIMEOUT_MS,
-  DEFAULT_NOTARIZATION_HEADERS,
-  DEFAULT_FETCH_OPTIONS,
-  DEFAULT_NOTARIZATION_OPTIONS,
-  DEFAULT_NOTARIZATION_BACKENDS,
-  DEFAULT_VERIFICATION_BACKEND,
-
-  AttestationError,
-  DebugAttestationError,
-  AttestationIntegrityError,
-
-  ClientConfig,
-  CustomBackendConfig,
-  CustomBackendAllowedFetchOptions,
-
-  AttestationRequest,
-  AttestationResponse,
-  DebugRequestResponse,
-  EnclaveInfo,
+  AttestationError, AttestationIntegrityError, AttestationRequest,
+  AttestationResponse, ClientConfig, CustomBackendAllowedFetchOptions, CustomBackendConfig, DebugAttestationError, DebugRequestResponse, DEFAULT_FETCH_OPTIONS, DEFAULT_NOTARIZATION_BACKENDS, DEFAULT_NOTARIZATION_HEADERS, DEFAULT_NOTARIZATION_OPTIONS, DEFAULT_TIMEOUT_MS, DEFAULT_VERIFICATION_BACKEND, EnclaveInfo,
   HeaderDict,
-  InfoOptions,
-  NotarizationOptions,
-  OracleData,
+  InfoOptions, NitroDocument, NitroInfo, NitroReportExtras, NotarizationOptions, OracleClient, OracleData,
   PositionInfo,
   ProofPositionalInfo,
-  SgxInfo,
-  NitroInfo,
-  NitroDocument,
-  NitroReportExtras,
+  SgxInfo, TlsConfig
 };
